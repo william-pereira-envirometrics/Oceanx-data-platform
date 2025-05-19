a67574eae2872732be01fad34c7e10ea891d85c4
william-pereira-envirometrics <william.pereira@enviro-metrics.co.uk>
2025-05-19 11:02:39 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import pathlib  # To handle folder paths
 import earthaccess  # To manage login to NASA Earthdata
 
-
+# Earthdata login credentials
+username = "will_pereira108"
+password = "Envirometrics2025?!"
 
 # Login to Earthdata and persist credentials
 auth = earthaccess.login(persist=True)
@@ -25,3 +27,12 @@ iron_release_date = "2024-12-28"
 # Format: (min_lon, min_lat, max_lon, max_lat)
 bbox = (-61.5, -53.2, -57.5, -50.9)
 
+# MySQL database connection settings
+db_config = {
+    "host": "db-mysql-lon1-78576-do-user-18592894-0.h.db.ondigitalocean.com",
+    "user": "doadmin",
+    "password": "AVNS_5d24Ut_RiknfWJtv9d6",
+    "database": "defaultdb",
+    "port": 25060,
+    "ssl": {"ssl": {}}
+}
